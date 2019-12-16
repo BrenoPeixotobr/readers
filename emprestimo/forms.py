efd2546773135e5086fb499b9a73fbf3df67835b
breno <breno.peixoto.santos@gmail.com>
2019-12-16 16:19:02 -0200

    erro de senha corrigido

--- emprestimo/forms.py
+++ emprestimo/forms.py
@@ -7,14 +7,14 @@ class PostEmprestimo(forms.ModelForm):
     #leitor = forms.CharField(label='Leitor')
     dataEmprestimo = forms.DateTimeField(label='Data de Emprestimo')
     dataPreDev = forms.DateTimeField(label='Data de Entrega')
-    dataEntrega = forms.DateTimeField(label='Data da Devolução',required=False)
+    password=forms.CharField(widget=forms.PasswordInput)
     #item=forms.IntegerField(label="Livro",widget=forms.TextInput(attrs={'readonly':'readonly'}))
     #bibliotecario=forms.CharField(label='Bibliotecario',widget=forms.TextInput(attrs={'readonly':'readonly'}))
 
 
     class Meta:
         model = Emprestimo
-        fields = ('leitor','dataEmprestimo', 'dataPreDev','dataEntrega','item','bibliotecario')
+        fields = ('leitor','dataEmprestimo', 'dataPreDev','item','bibliotecario')
 
 class PostEntrega(forms.Form):
     data=forms.DateTimeField(label='Data da Devolução',input_formats=['%d/%m/%Y'],widget=forms.DateTimeInput(attrs={'class': 'form-control datetimepicker-input','data-target': '#datetimepicker1'}))
--- emprestimo/views.py
+++ emprestimo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.db.models import Subquery
+from django.shortcuts import get_list_or_404, get_object_or_404
 from django import forms
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from .models import Emprestimo
@@ -20,15 +23,28 @@ def emprestar(request,id):
         if request.method == "POST":
             form = PostEmprestimo(request.POST)
             if form.is_valid():
-                post = form.save(commit=False)
-                Item.objects.filter(idItem=id).update(status="E")
-                liv=Item.objects.filter(idItem=id).values("livbib")
-                c=str(liv)
+                user=Usuario.objects.filter(CPF=form['leitor'].value()).values('user_id')
+                c=str(user)
                 c2=c.split(': ')
                 c3=c2[1].split("}")
-                livbib=c3[0]
-                post.save()
-                return HttpResponseRedirect('../../item/lista/'+livbib)
+                user = get_object_or_404(User, pk=c3[0])
+                autorizado = authenticate(username=user,password=form['password'].value())
+                if autorizado:
+                    post = form.save(commit=False)
+                    Item.objects.filter(idItem=id).update(status="E")
+                    liv=Item.objects.filter(idItem=id).values("livbib")
+                    c=str(liv)
+                    c2=c.split(': ')
+                    c3=c2[1].split("}")
+                    livbib=c3[0]
+                    post.save()
+                    return HttpResponseRedirect('../../item/lista/'+livbib)
+                else:
+                    mensagem_de_erro="Leitor ou senha invalidos!"
+                    contexto = {
+                        'mensagem_de_erro': mensagem_de_erro
+                        }
+                    return render(request, "emprestimo/erros.html", contexto)
             else:
                 return render_to_response("erros/erro_form.html",{'form': form})
         else:
